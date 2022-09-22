e3b9d420b1e70410091ff82623f4461de7f7e689
Enrique <enrique_.00@hotmail.com>
2022-09-22 01:34:48 -0500

    Update link for images people

--- src/components/people/People.js
+++ src/components/people/People.js
@@ -7,7 +7,7 @@ export const People = ({people}) => {
   const {known_for_department, name, profile_path} = people
   return (
     <li>
-      <PeopleProfile image={`http://image.tmdb.org/t/p/original/${profile_path}`}>
+      <PeopleProfile image={`http://image.tmdb.org/t/p/w500/${profile_path}`}>
         <h2>{name}</h2>
         <h4>{known_for_department}</h4>
       </PeopleProfile>
