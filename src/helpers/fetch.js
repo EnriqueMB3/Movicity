fe1450bd9f2db31e62f43eeffcab00feecda1020
Enrique <enrique_.00@hotmail.com>
2022-10-03 23:50:34 -0500

    Headers content type

--- src/helpers/fetch.js
+++ src/helpers/fetch.js
@@ -2,7 +2,13 @@ const baseUrl = process.env.REACT_APP_API_URL;
 const tokenUrl = process.env.REACT_APP_API_TOKEN;
 
 const fetchConToken = async(endpoint, method = 'GET', language='en-US', page='1' ) => {
-    const req = await fetch(`${baseUrl}${endpoint}?api_key=${tokenUrl}&page=${page}`);
+    const req = await fetch(`${baseUrl}${endpoint}?api_key=${tokenUrl}&page=${page}`,
+        {
+            method,
+            headers: {
+                'Content-type': 'application/json',
+            }
+        });
     const json = await req.json();
     return json;
 }
